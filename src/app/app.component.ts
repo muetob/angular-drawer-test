c2bde5c57109f58aa1fd90a85c8dc414f356b3d0
Tobias Mueller <tobias.mueller@hostpoint.ch>
2021-09-15 07:46:32 +0200

    Simplify

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,18 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  template: `
+    <div>
+      Detail page
+
+      <app-user-detail></app-user-detail>
+    </div>
+
+
+    <app-drawer>
+      <app-user-detail></app-user-detail>
+    </app-drawer>
+  `
 })
 export class AppComponent {
   title = 'ant-playground';
